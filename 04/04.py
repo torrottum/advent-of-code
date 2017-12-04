191b7b6be2d6c49f79dea2e0d9353e0656d5267f
Tor Røttum <tor@rottum.me>
2017-12-04 10:58:46 +0100

    use set for part 2 as well

--- 04/04.py
+++ 04/04.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 import os
 
-def check_phrase(words):
-    l = len(words)
-    x = l * -1
-    for i in range(l):
-        for j in range(l):
-            if sorted(words[i]) == sorted(words[j]):
-                x += 1
-    return not x > 0
-
 with open(os.path.dirname(os.path.realpath(__file__)) + '/input.txt') as f:
     p1 = 0
     p2 = 0
@@ -23,8 +14,9 @@ with open(os.path.dirname(os.path.realpath(__file__)) + '/input.txt') as f:
         if len(words) == len(set(words)):
             p1 += 1
 
-        if check_phrase(words):
+        x=[''.join(sorted(i)) for i in line.split(' ')]
+        if len(x) == len(set(x)):
             p2 += 1
-            
+
     print('Part 1: {}'.format(p1))
     print('Part 2: {}'.format(p2))
